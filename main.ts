a42c8f91e72919b96863cf013fdf0cf8bd527f50
dantef12 <100299384+dantef12@users.noreply.github.com>
2022-03-02 20:45:11 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    number_1 += 1
+    basic.showNumber(number_1)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+    basic.showNumber(0)
+    number_1 = 0
+})
+input.onButtonPressed(Button.B, function () {
+    number_1 += -1
+    basic.showNumber(number_1)
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+let number_1 = 0
+number_1 = 0
+let number_2 = 0
 basic.forever(function () {
 	
 })
